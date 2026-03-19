422078cde0541a4985ee8b72937669d387c60302
Bancharka <olipet101@gmail.com>
2026-03-19 13:52:57 +0100

    Created new views, deleted obselete stuff

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,2 @@
-<script setup>
-import MyFetch from "@/components/MyFetch.vue";
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
-
-<template>
-  <main>
-    <TheWelcome />
-    <MyFetch />
-    <h1>Hejs</h1>
-  </main>
-</template>
+<script script></script>
+<template></template>
---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import MyFetch from "@/components/MyFetch.vue";
+</script>
+
+<template>
+  <main>
+    <MyFetch />
+    <h1>Hejs</h1>
+  </main>
+</template>
