15ec8ecf379785d7c5fa5ee01a023ad2ae20ac8e
Clara la Cour <denlacour@gmail.com>
2026-03-24 11:27:09 +0100

    container fix

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,11 @@ import { RouterLink, RouterView } from "vue-router";
   <div class="phone-wrapper">
     <div class="mobile-container">
       <div class="page-container">
+        <img
+          src="@/assets/status-bar.png"
+          alt="Phone status bar"
+          class="page-container__phone-status"
+        />
         <RouterView />
       </div>
     </div>
