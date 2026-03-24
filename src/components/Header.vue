73a8377a5d30efdd578839c02d48e3a33fd1be3c
Clara la Cour <denlacour@gmail.com>
2026-03-24 08:18:43 +0100

    work on header and import fonts

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { computed } from "vue";
+import { useRoute, useRouter } from "vue-router";
+
+const route = useRoute();
+const router = useRouter();
+
+const isHome = computed(() => route.name === "home" || route.name === "welcome");
+const title = computed(() => route.meta.title);
+</script>
+
+<template>
+  <button v-if="!isHome" @click="router.back()">
+    <img src="@/assets/icons/back-arrow.svg" alt="back-arrow" />
+  </button>
+  <h1>{{ title }}</h1>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,13 @@ const router = createRouter({
       component: WelcomeView,
     },
     {
-      path: "/home",
+      path: "/dev-home",
       name: "home",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/HomeView.vue"),
+      component: () => import("../views/developers/HomeView.vue"),
+      meta: { title: 'Hjem' }
     },
     {
       path: "/login",
