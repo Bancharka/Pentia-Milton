726ab4797ebf23ac23f42e7ce81ba4e44f2461e3
Melledodo <mkaagaard@hotmail.com>
2026-03-26 19:00:41 +0100

    Login input setup

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import BaseButton from "@/components/BaseButton.vue";
+import LoginInput from "@/components/LoginInput.vue";
 </script>
 <template>
   <BaseButton text="Klik" variant="primary" link="/home" />
@@ -7,4 +8,6 @@ import BaseButton from "@/components/BaseButton.vue";
   <BaseButton text="På" variant="secondary" link="/about" />
 
   <BaseButton text="Mig" variant="outline" link="/contact" />
+
+  <LoginInput />
 </template>
