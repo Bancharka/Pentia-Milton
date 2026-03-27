54259105290d552d4114de6269407bca9a7f74ac
Melledodo <mkaagaard@hotmail.com>
2026-03-27 13:01:19 +0100

    login input fixes

--- src/components/LoginInput.vue
+++ src/components/LoginInput.vue
@@ -1,12 +1,42 @@
 <template>
-  <div class="login-input">
-    <div class="input-field">
-      <span class="icon">✉️</span>
+  <div class="login">
+    <div class="login__field">
+      <span class="login__icon">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          stroke="#004552"
+          fill="none"
+          width="20"
+          height="20"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+          />
+        </svg>
+      </span>
       <input type="email" placeholder="Mail" v-model="email" />
     </div>
 
-    <div class="input-field">
-      <span class="icon">🔑</span>
+    <div class="login__field">
+      <span class="login__icon">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          stroke="#004552"
+          fill="none"
+          width="20"
+          height="20"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z"
+          />
+        </svg>
+      </span>
       <input type="password" placeholder="Adgangskode" v-model="password" />
     </div>
   </div>
