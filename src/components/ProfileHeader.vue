cde2c944a6e1cec4ab4901ca880f326b257f2cfd
Clara la Cour <denlacour@gmail.com>
2026-03-27 14:13:35 +0100

    profile-header done

--- src/components/ProfileHeader.vue
+++ src/components/ProfileHeader.vue
@@ -1,6 +1,10 @@
 <script setup></script>
 <template>
   <div class="profile-header">
-    <img src="" alt="" />
+    <img class="profile-header__img" src="@/assets/img/builder.png" alt="Profilbillede" />
+    <div class="profile-header__text">
+      <h3>Thomas Jensen</h3>
+      <p>thomas_jensen@milton.dk</p>
+    </div>
   </div>
 </template>
--- src/views/developers/HomeView.vue
+++ src/views/developers/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Header from "@/components/Header.vue";
 import HouseCard from "@/components/HouseCard.vue";
+import ProfileHeader from "@/components/ProfileHeader.vue";
 </script>
 
 <template>
   <Header />
+  <ProfileHeader />
   <HouseCard />
   <HouseCard />
 </template>
