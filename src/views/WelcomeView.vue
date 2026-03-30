5829d3ef80f2adf62020b3dfc879e9e61571e2a2
Clara la Cour <denlacour@gmail.com>
2026-03-30 16:41:30 +0200

    kinda broken

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,9 +1,10 @@
-<script setup></script>
-
 <template>
   <div class="welcome">
-    <img src="@/assets/img/house3.png" alt="hus" />
-    <div class="page-container"></div>
-    <h1>Hejs</h1>
+    <img src="@/assets/img/house3.png" alt="hus" class="welcome__background" />
+    <img src="@/assets/icons/logo.png" alt="" class="welcome__logo" />
+    <div class="welcome__container">
+      <h1 class="welcome__title">Velkommen</h1>
+      <!-- your inputs go here -->
+    </div>
   </div>
 </template>
