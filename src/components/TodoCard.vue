f6a5c9b77218eab6557ecdf252066069136cdb13
Melledodo <mkaagaard@hotmail.com>
2026-03-31 22:27:18 +0200

    TodoCard setup

--- src/components/TodoCard.vue
+++ src/components/TodoCard.vue
@@ -1,5 +1,28 @@
 <template>
-<div class="todocard">
-    
-</div>
+  <label class="todo-card" :class="{ 'todo-card--checked': isChecked }">
+    <input v-model="isChecked" type="checkbox" class="todo-card__checkbox" />
+
+    <span class="todo-card__text">{{ task }}</span>
+  </label>
 </template>
+
+<script>
+export default {
+  name: "TodoCard",
+  props: {
+    task: {
+      type: String,
+      required: true,
+    },
+    checked: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      isChecked: this.checked,
+    };
+  },
+};
+</script>
