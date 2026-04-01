29d15c30ec8323e00c172239f3eb4850bbf2e46b
Bancharka <olipet101@gmail.com>
2026-04-01 13:14:50 +0200

    Progressbar computing
    I have made a temporary array, which is then used to calculate what percentage to fill the bar. This is calculated from a custom max %

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,4 +1,12 @@
-<script setup></script>
+<script setup>
+import { ref, computed } from "vue";
+let mitArray = ref([1, 2, 3, 4, 5, 6]);
+
+const maxArray = 6;
+
+const fillPercent = computed(() => (mitArray.value.length / maxArray) * 100);
+console.log(fillPercent);
+</script>
 
 <template>
   <div class="progress-bar">
@@ -7,6 +15,6 @@
       src="../img/progressbar_overlay_100_icons_lightergreen.svg"
       alt=""
     />
-    <div class="progress_color"></div>
+    <div class="progress_color" :style="{ width: fillPercent + '%' }"></div>
   </div>
 </template>
