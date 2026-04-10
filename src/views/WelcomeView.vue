a752c2e7f440972edd20de1ed2b194ce3d4b68f9
Clara la Cour <denlacour@gmail.com>
2026-04-10 12:39:42 +0200

    welcome done

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,11 +1,16 @@
+<script setup>
+import BaseButton from "@/components/BaseButton.vue";
+</script>
+
 <template>
-  <div class="page-container">
-    <div class="welcome">
-      <img src="@/assets/img/house3.png" alt="hus" class="welcome__background" />
-      <img src="@/assets/icons/logo.png" alt="" class="welcome__logo" />
-      <div class="welcome__container">
+  <img src="@/assets/img/house3.png" alt="hus" class="welcome__background" />
+  <div class="welcome">
+    <img src="@/assets/icons/logo.png" alt="" class="welcome__logo" />
+    <div class="welcome__container">
+      <div class="page-container">
         <h1 class="welcome__title">Velkommen</h1>
-        <!-- your inputs go here -->
+        <BaseButton variant="secondary" text="Bygherre" link="/home" />
+        <BaseButton variant="outline" text="Bygleder" link="/home" />
       </div>
     </div>
   </div>
