edcdb4d509781f903306c57c2c5a195e9262264a
Clara la Cour <denlacour@gmail.com>
2026-04-14 09:43:17 +0200

    login done

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,7 +3,7 @@ import BaseButton from "@/components/BaseButton.vue";
 import LoginInput from "@/components/LoginInput.vue";
 </script>
 <template>
-  <img src="@/assets/img/house4.png" alt="hus" class="welcome__background" />
+  <img src="@/assets/img/house4.png" alt="hus" class="login__background" />
   <div class="login">
     <h1 class="login__title">Kom i gang</h1>
     <div class="login__container">
