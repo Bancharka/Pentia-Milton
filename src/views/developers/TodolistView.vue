927d69a02f0c3e1f3abc153bdcdfbe14f83bbe73
Melledodo <mkaagaard@hotmail.com>
2026-04-14 13:23:28 +0200

    Update TodolistView.vue

--- src/views/developers/TodolistView.vue
+++ src/views/developers/TodolistView.vue
@@ -2,12 +2,14 @@
 import Header from "@/components/Header.vue";
 import BottomNav from "@/components/BottomNav.vue";
 import TodoCard from "@/components/TodoCard.vue";
+import SearchInput from "@/components/SearchInput.vue";
 </script>
 
 <template>
   <div class="page-container">
     <Header />
     <div class="site-container site-container--secondary">
+      <SearchInput placeholder="Search" />
       <div class="todo-list">
         <TodoCard task="Drik en monster" />
         <TodoCard task="Spis et ur" />
