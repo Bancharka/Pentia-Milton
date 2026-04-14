c4d6c16d68c1b4407f1d3530e02daf9f06d43090
Clara la Cour <denlacour@gmail.com>
2026-04-14 13:15:33 +0200

    overviewviewvue done

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  placeholder: {
+    type: String,
+    default: "Søg...",
+  },
+});
+</script>
+<template>
+  <div class="searchinput">
+    <span>
+      <img
+        src="@/assets/icons/magnifyingglass.svg"
+        alt="Forstørrelsesglas"
+        class="searchinput__icon"
+      />
+    </span>
+    <input type="text" :placeholder="placeholder" />
+  </div>
+</template>
