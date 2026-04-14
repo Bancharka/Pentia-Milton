a58754b3d0c393bd35d48ebecb0c216ff0ac6542
Melledodo <mkaagaard@hotmail.com>
2026-04-14 12:17:42 +0200

    todolistview updated

--- src/views/developers/TodolistView.vue
+++ src/views/developers/TodolistView.vue
@@ -1,12 +1,23 @@
 <script setup>
 import Header from "@/components/Header.vue";
 import BottomNav from "@/components/BottomNav.vue";
+import TodoCard from "@/components/TodoCard.vue";
 </script>
 
 <template>
   <div class="page-container">
     <Header />
-    <div class="site-container site-container--secondary"></div>
+    <div class="site-container site-container--secondary">
+      <div class="todo-list">
+        <TodoCard task="Drik en monster" />
+        <TodoCard task="Spis et ur" />
+        <TodoCard task="Gør noget dumt" />
+        <TodoCard task="Mob Clara" />
+        <TodoCard task="Tegn en tegning" />
+        <TodoCard task="Grib en ært" />
+        <TodoCard task="Byg en urte" />
+      </div>
+    </div>
     <BottomNav />
   </div>
 </template>
