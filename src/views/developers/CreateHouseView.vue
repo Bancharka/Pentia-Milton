f372a1f93094cd67b6cda482b4a8687bdc0d9479
Clara la Cour <denlacour@gmail.com>
2026-04-20 10:20:12 +0200

    start input

--- src/views/developers/CreateHouseView.vue
+++ src/views/developers/CreateHouseView.vue
@@ -1,7 +1,12 @@
-<script setup></script>
+<script setup>
+import Header from "@/components/Header.vue"
+</script>
 
 <template>
+  <div class="page-container">
+  <Header />
   <form action="submit">
-    <input type="text" />
+    <input type="text" placeholder="Adresse" />
   </form>
+</div>
 </template>
