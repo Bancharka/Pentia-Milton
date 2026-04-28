644c3e2f8c3cdcfff76b926c8c86eff9209da604
Clara la Cour <denlacour@gmail.com>
2026-04-28 12:33:16 +0200

    build overview

--- src/views/customers/BuildOverviewView.vue
+++ src/views/customers/BuildOverviewView.vue
@@ -17,27 +17,27 @@ onMounted(async() => {
         <Header />
         <div class="site-container site-container--primary">
             <div class="build-overview">
-                <div
-                v-for="(todo, index) in store.todos"
-                key="index"
-                >
-                <div class="build-overview__indicator">
-                    
-                </div>
-                <div class="build-overview__content">
-                    <h3 class="build-overview__title">
-                        {{ todo.title }}
-                    </h3>
-                    <ul class="build-overview__subtodos">
-                        <li
-                        v-for="(sub, subIndex) in todo.subTodos"
-                        :key="subIndex"
-                        >{{ sub.title }}
-                        </li>
-                    </ul>
-                </div>
-                </div>
-            </div>
+    <div
+      v-for="(todo, index) in store.todos"
+      :key="index"
+      class="build-overview__item"
+    >
+        <div class="build-overview__indicator">
+            <img v-if="todo.done" class="build-overview__icon" src="@/assets/icons/done.png" alt="done" />
+            <img v-else-if="todo.subTodos.some(s => s.done)" class="build-overview__icon" src="@/assets/icons/doing.png" alt="active" />
+            <img v-else class="build-overview__icon" src="@/assets/icons/todo.png" alt="todo" />
+                <div class="build-overview__line"></div>
+        </div>
+        <div class="build-overview__content">
+            <h3 class="build-overview__title">{{ todo.title }}</h3>
+            <ul class="build-overview__subtodos">
+                <li v-for="(sub, subIndex) in todo.subTodos" :key="subIndex">
+                    {{ sub.title }}
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
         </div>
         <BottomNav />
     </div>
