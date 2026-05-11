a8173a93a4051af1e93d1a614499ab839a4d6ba2
Bancharka <153490604+Bancharka@users.noreply.github.com>
2026-05-11 17:33:51 +0200

    Tilføj flere e2e tests

--- cypress/e2e/customerOverview.cy.js
+++ cypress/e2e/customerOverview.cy.js
@@ -7,13 +7,33 @@ describe('overview', () => {
 
     it('should be on /home-customer', () => {
 
+        // Tjekker url om den er på home customer
         cy.url().should('eq', 'http://localhost:5173/home-customer' )
 
+        // Tjekker for om der er en header med "hjem"
         cy.get('.header').should('contain', 'Hjem')
 
     });
 
+    it('should check the progressbar has a width of 142.71875px == 34.1463%', () => {
 
+        // Tjekker om progressbaren har en bredde
+        cy.get('.house-card__progress-bar__fill').should('have.css', 'width','142.71875px')
+
+    });
+
+    it('should have an illustration of the house', () => {
+
+    //Tjekker om billedet på siden er en af de fire billeder der kan være
+    cy.get('.house-card img').invoke('attr', 'src').then((src) => {
+        expect(src).to.be.oneOf([
+            '/src/img/House-walls.png',
+            '/src/img/House-foundation.png',
+            '/src/img/House-walls-roof.png',
+            '/src/img/House-done.png'
+        ])
+    })
+})
 
 
 
