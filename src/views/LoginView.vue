f1e12d39f5641a6a68206f8d076dc6957bea5210
Bancharka <153490604+Bancharka@users.noreply.github.com>
2026-05-14 13:51:51 +0200

    fix loading on custumer view, add documents nav

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,12 +2,14 @@
 import LoginInput from '@/components/LoginInput.vue'
 </script>
 <template>
-    <img src="@/assets/img/house4.png" alt="hus" class="login__background" >
-    <div class="login">
-        <h1 class="login__title">Kom i gang</h1>
-        <div class="login__container">
-            <div class="login__input">
-                <LoginInput />
+    <div>
+        <img src="@/assets/img/house4.png" alt="hus" class="login__background" >
+        <div class="login">
+            <h1 class="login__title">Kom i gang</h1>
+            <div class="login__container">
+                <div class="login__input">
+                    <LoginInput />
+                </div>
             </div>
         </div>
     </div>
--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -2,14 +2,16 @@
 import BaseButton from '@/components/BaseButton.vue'
 </script>
 <template>
-    <img src="@/assets/img/house3.png" alt="hus" class="welcome__background" >
-    <div class="welcome">
-        <img src="@/assets/icons/logo.png" alt="" class="welcome__logo" >
-        <div class="welcome__container">
-            <div class="page-container">
-                <h1 class="welcome__title">Velkommen</h1>
-                <BaseButton variant="secondary" text="Bygherre" link="/login" />
-                <BaseButton variant="outline" text="Byggeleder" link="/login" />
+    <div>
+        <img src="@/assets/img/house3.png" alt="hus" class="welcome__background" >
+        <div class="welcome">
+            <img src="@/assets/icons/logo.png" alt="" class="welcome__logo" >
+            <div class="welcome__container">
+                <div class="page-container">
+                    <h1 class="welcome__title">Velkommen</h1>
+                    <BaseButton variant="secondary" text="Bygherre" link="/login" />
+                    <BaseButton variant="outline" text="Byggeleder" link="/login" />
+                </div>
             </div>
         </div>
     </div>
